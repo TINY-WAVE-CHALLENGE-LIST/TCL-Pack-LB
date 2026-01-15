cac68f43994afeaaa4b379864fe6399c3d2563fb
zis76 <76632633+zis76@users.noreply.github.com>
2026-01-15 12:00:35 -0600

    Create incompletedpacks.js

---
+++ js/incompletedpacks.js
@@ -0,0 +1,61 @@
+if (rank = 1) {
+        return 200;
+    }
+    if (rank = 2) {
+        return 500;
+    }
+    if (rank = 3) {
+        return 350;
+    }
+    if (rank = 4) {
+        return 250;
+    }
+    if (rank = 5) {
+        return 650;
+    }
+    if (rank = 6) {
+        return 300;
+    }
+    if (rank = 7) {
+        return 550;
+    }
+    if (rank = 8) {
+        return 400;
+    }
+    if (rank = 9) {
+        return 150;
+    }
+    if (rank = 10) {
+        return 350;
+    }
+    if (rank = 11) {
+        return 700;
+    }
+    if (rank = 12) {
+        return 300;
+    }
+    if (rank = 13) {
+        return 350;
+    }
+    if (rank = 14) {
+        return 500;
+    }
+    if (rank = 15) {
+        return 500;
+    }
+    if (rank = 16) {
+        return 750;
+    }
+    if (rank = 17) {
+        return 650;
+    }
+    if (rank = 18) {
+        return 450;
+    }
+    if (rank = 19) {
+        return 600;
+    }
+    if (rank = 20) {
+        return 1000;
+    }
+}
